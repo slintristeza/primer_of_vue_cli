a69fc4f386136d3e424c82bb8b56c2caa48d35cd
slintristeza <slintristeza@gmail.com>
2019-06-02 23:48:29 +0900

    feat windowPlugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import windowPlugin from './windowPlugin'
 
 Vue.config.productionTip = false
+
+Vue.use(windowPlugin)
 
 /* eslint-disable no-new */
 new Vue({
