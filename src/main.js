eb8c9e838ce346ebafc1439977d729539f0cd3a0
slintristeza <slintristeza@gmail.com>
2019-06-04 03:33:45 +0900

    installed vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import windowPlugin from './windowPlugin'
+import 'babel-polyfill'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
 Vue.use(windowPlugin)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
