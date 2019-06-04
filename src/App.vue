372294d18466d3d8238ce7aed218bcca8a8e05e3
slintristeza <slintristeza@gmail.com>
2019-06-04 17:55:43 +0900

    section43 258-260

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h3>引数なし</h3>
+    <ol>
+      <li>{{ count }}</li>
+      <li>{{ max }}</li>
+    </ol>
+    <h3>引数付き</h3>
+    <ol>
+      <li>{{ itemA }}</li>
+      <li>{{ itemB(1) }}</li>
+      <li>{{ nameA }}</li>
+      <li>{{ nameB(1) }}</li>
+    </ol>
   </div>
 </template>
 
@@ -19,6 +29,16 @@ export default {
     console.log(this.$store.state.count)
     this.$store.commit('decrement')
     console.log(this.$store.state.count)
+  },
+  computed: {
+    // 引数なしゲッター
+    count () { return this.$store.getters.count },
+    max () { return this.$store.getters.max },
+    // 引数付きゲッター
+    itemA () { return this.$store.getters.item(1) }, // good
+    itemB () { return this.$store.getters.item }, // bad
+    nameA () { return this.$store.getters.name(1) }, // good
+    nameB () { return this.$store.getters.name } // bad
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -6,13 +6,37 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 300 }
+    ]
   },
   mutations: {
     increment (state) {
       state.count++
     },
-    decrement: state => [ state.count-- ]
+    decrement: state => [state.count--],
+    mutationType (state, payload) {
+      state.count = payload
+    }
+  },
+  getters: {
+    count (state, getters, rootState, rootGetter) {
+      return state.count
+    },
+    max (state) {
+      return state.list.reduce((a, b) => {
+        return a > b.price ? a : b.price
+      }, 0)
+    },
+    item (state) {
+      return id => state.list.find(el => el.id === id)
+    },
+    name (state, getters) {
+      return id => getters.item(id).name
+    }
   }
 })
 export default store
