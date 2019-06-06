451c3ec9a921010d1d7d69033e01377cff32b67d
slintristeza <slintristeza@gmail.com>
2019-06-07 03:33:24 +0900

    fix: Product Home

--- src/views/Product/Home.vue
+++ src/views/Product/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="product-home">
     <h1>商品情報</h1>
-    <p>ID.{{ id }}の詳細を表示</p>
+    <p>ID.{{ detail.id }}の詳細を表示</p>
     <div class="product-table">
       <dt>商品名</dt><dd>{{ detail.name }}</dd>
       <dt>価格</dt><dd>{{ detail.price }}</dd>
@@ -13,17 +13,6 @@
 import { mapGetters } from 'vuex'
 export default {
   props: { id: Number },
-  computed: mapGetters('product', ['detail']),
-  watch: {
-    id: {
-      handler () {
-        this.$store.dispatch('product/load', this.id)
-      },
-      immediate: true
-    }
-  },
-  breforeDestroy () {
-    this.$store.dispatch('product/destroy')
-  }
+  computed: mapGetters('product', ['detail'])
 }
 </script>
