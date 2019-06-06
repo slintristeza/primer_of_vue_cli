7ed1090a7eee4c0dc0598cbdb24484a2dbf4150f
slintristeza <slintristeza@gmail.com>
2019-06-06 16:33:00 +0900

    section51

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '@/views/Home'
+import ProductList from '@/views/ProductList'
 import Product from '@/views/Product'
 import Button from '@/views/Button'
 
@@ -10,9 +11,24 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { name: 'product', path: '/Product/:id', component: Product },
-    { name: 'button', path: '/Button', component: Button }
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/Product',
+      component: ProductList
+    },
+    {
+      path: '/Product/:id(\\d+)', // 数字のみIDにマッチ
+      component: Product,
+      props: route => ({ id: Number(route.params.id) })
+    },
+    {
+      name: 'button',
+      path: '/Button',
+      component: Button
+    }
   ]
 })
 
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="product">
+  <div class="product" v-if="item" key="product">
     <h1>商品情報</h1>
+    <p>ID.{{ id }}の詳細を表示</p>
+    <div class="product-table">
+      <dt>商品名</dt><dd>{{ item.name }}</dd>
+      <dt>価格</dt><dd>{{ item.price }}</dd>
+      <dt>商品説明</dt><dd>{{ item.content }}</dd>
+    </div>
   </div>
+  <div v-else key="loading">商品情報を読み込んでいます</div>
 </template>
+<script>
+import products from '@/api/products.js'
+export default {
+  props: { id: Number },
+  data () {
+    return {
+      item: null
+    }
+  },
+  watch: {
+    id: {
+      handler () {
+        products.asyncFind(this.id, item => {
+          this.item = item
+        })
+      },
+      immediate: true
+    }
+  }
+}
+</script>
+
+<style>
+product-table {
+  display: flex;
+}
+</style>
